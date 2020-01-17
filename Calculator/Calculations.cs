79161186c05f13ac4edaf23781667fc02380ccba
mikearimas <mikearimas@gmail.com>
2020-01-17 14:20:00 -0800

    Initial Commit

---
+++ Calculator/Calculations.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Calculations
+    {
+        public static double Addition(double num, double num2)
+        {
+            return num + num2;
+        }
+        public static double Subtraction(double num, double num2)
+        {
+            return num - num2;
+        }
+
+        public static double Division(double num, double num2)
+        {
+            return (num2 != 0) ? num / num2 : 0;
+        }
+
+        public static double Multiplication(double num,double num2)
+        {
+            return (num2 != 0) ? num * num2 : 0;
+        }
+    }
+}
+
---
+++ Calculator/main_calculator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class main_calculator : Form
+    {
+        public main_calculator()
+        {
+            InitializeComponent();
+        }
+    }
+}
